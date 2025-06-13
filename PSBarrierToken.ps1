b57024cbdecc6342cc894c18cb160017caf05cb4
Akhil Grandhi <55430217+AkhilGrandhi@users.noreply.github.com>
2025-06-13 18:53:53 +0530

    Add files via upload

---
+++ PSBarrierToken.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module MicrosoftPowerBIMgmt
+Login-PowerBIServiceAccount
+$headers = Get-PowerBIAccessToken
+$bearerToken = $headers["Authorization"]
+
+# If the bearer token includes the 'Bearer' prefix, and you want to remove it:
+$bearerToken = $bearerToken -replace 'Bearer ', ''
+
+# Output the bearer token
+Write-Output $bearerToken
+
+
